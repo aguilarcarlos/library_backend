079e042c70a7a0a6337a7c3bfdcad64c5ca1e0c2
Carlos David Aguilar Ruiz <carlosaguilarnet@gmail.com>
2016-12-18 01:42:51 -0600

    modify domain

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ const envs = {
     PRODUCTION: {
         ENV_NAME: 'PRODUCTION',
         PORT: process.env.NODE_PORT || 3000,
-        DOMAIN: 'api_demo.aguilarcarlos.com',
+        DOMAIN: 'api.demo.aguilarcarlos.com',
         IP: '127.0.0.1',
         DATABASE: process.env.DATABASE || 'mongodb://library:sandbox@ds147377.mlab.com:47377/library'
     },
